46dd85b861ce0bc2050c73ebf4f985fa90c70ad9
Aleksandar Ristic <storiaca@gmail.com>
2019-02-01 14:20:50 +0100

    Working with ejs

--- 06-Working with Dynamic Content and Adding Templating Engines/app.js
+++ 06-Working with Dynamic Content and Adding Templating Engines/app.js
@@ -2,12 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'pug');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -23,4 +21,4 @@ app.use((req, res, next) => {
   res.status(404).render('404', {pageTitle: '404 Page Not Found'});
 });
 
-app.listen(3000);
+app.listen(4000);
