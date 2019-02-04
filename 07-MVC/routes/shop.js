2d69ac449a0fee3decce3b25414c8d5ba218421b
Aleksandar Ristic <storiaca@gmail.com>
2019-02-04 12:19:01 +0100

    Creating error controller

--- 07-MVC/routes/shop.js
+++ 07-MVC/routes/shop.js
@@ -6,6 +6,6 @@ const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', productsController.getAddProduct);
+router.get('/', productsController.getProducts);
 
 module.exports = router;
