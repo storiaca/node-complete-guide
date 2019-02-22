ab70e0bb3cac20bbda5d18df57c03335afbd3fd2
Aleksandar Ristic <storiaca@gmail.com>
2019-02-22 16:52:43 +0100

    Sequelize start

---
+++ 11-Sequelize/util/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize();
+
+// host: 'localhost',
+//   user: 'root',
+//   database: 'node-complete',
+//   password: 'kurs2019'
