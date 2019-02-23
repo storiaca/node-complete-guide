fbc25bf81177946a961383ba94baec2cf1c33134
storiaca <storiaca@gmail.com>
2019-02-23 13:09:45 +0100

    Create connection to mysql database

--- 11-Sequelize/util/database.js
+++ 11-Sequelize/util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize();
+const sequelize = new Sequelize("node-complete", "root", "root123", {
+  dialect: "mysql",
+  host: "localhost"
+});
 
+module.exports = sequelize;
 // host: 'localhost',
 //   user: 'root',
 //   database: 'node-complete',
 //   password: 'kurs2019'
+
+// pass: 'root123'
