2da39bcbd5aaf97a75562e02ac23ccbe5aab1c78
Aleksandar Ristic <storiaca@gmail.com>
2019-02-25 16:25:47 +0100

    Insert data and create product with sequelize

--- 11-Sequelize/util/database.js
+++ 11-Sequelize/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("node-complete", "root", "root123", {
+const sequelize = new Sequelize("node-complete", "root", "kurs2019", {
   dialect: "mysql",
   host: "localhost"
 });
