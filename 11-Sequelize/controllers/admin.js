9d18f76eb56209977597ae072a109f915d7129da
Aleksandar Ristic <storiaca@gmail.com>
2019-02-27 13:40:13 +0100

    Fetching related products

--- 11-Sequelize/controllers/admin.js
+++ 11-Sequelize/controllers/admin.js
@@ -13,18 +13,20 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  }).then(result => {
-    //console.log(result);
-    console.log('Created Product');
-    res.redirect('/admin/products')
-  }).catch(err => {
-    console.log(err);
-  });
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description
+    })
+    .then(result => {
+      //console.log(result);
+      console.log('Created Product');
+      res.redirect('/admin/products')
+    }).catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
