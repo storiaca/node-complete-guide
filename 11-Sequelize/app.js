f8db3b76da9443abb9bd7ae234764c569d4add87
Aleksandar Ristic <storiaca@gmail.com>
2019-02-28 10:59:09 +0100

    Creating and fetching a cart

--- 11-Sequelize/app.js
+++ 11-Sequelize/app.js
@@ -45,8 +45,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
-  //.sync()
+  //.sync({ force: true })
+  .sync()
   .then(result => {
     return User.findByPk(1)
     //console.log(result);
@@ -59,6 +59,9 @@ sequelize
   })
   .then(user => {
     //console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
     app.listen(3000);
   })
   .catch(err => {
