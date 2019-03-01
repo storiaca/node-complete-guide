aebee77d05e6f95274e802795d7ffd7c5d21ef02
Aleksandar Ristic <storiaca@gmail.com>
2019-03-01 10:44:34 +0100

    Storing Cartitems as Orderitems

--- 11-Sequelize/app.js
+++ 11-Sequelize/app.js
@@ -50,8 +50,8 @@ User.hasMany(Order);
 Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
-  .sync({ force: true })
-  //.sync()
+  //.sync({ force: true })
+  .sync()
   .then(result => {
     return User.findByPk(1)
     //console.log(result);
