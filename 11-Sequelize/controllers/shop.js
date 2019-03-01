225017abdf31566c50e768a953fff2d5f86e54bd
Aleksandar Ristic <storiaca@gmail.com>
2019-03-01 14:14:36 +0100

    Resetting the Cart and Fetching and outputing orders

--- 11-Sequelize/controllers/shop.js
+++ 11-Sequelize/controllers/shop.js
@@ -121,8 +121,10 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user.getCart()
     .then(cart => {
+      fetchedCart = cart;
       return cart.getProducts()
     })
     .then(products => {
@@ -136,21 +138,24 @@ exports.postOrder = (req, res, next) => {
         .catch(err => console.log(err))
     })
     .then(result => {
+      return fetchedCart.setProducts(null)
+    })
+    .then(result => {
       res.redirect('/orders')
     })
     .catch(err => console.log(err))
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user.getOrders({include: ['products']})
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders
+      });
+    })
+    .catch(err => console.log(err))
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
-};
+
