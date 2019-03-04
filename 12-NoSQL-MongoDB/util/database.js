2ece95af40ab6ecc7ccdd1b7fa0924a1504d0aea
Aleksandar Ristic <storiaca@gmail.com>
2019-03-04 15:06:10 +0100

    Creating the database connection

--- 12-NoSQL-MongoDB/util/database.js
+++ 12-NoSQL-MongoDB/util/database.js
@@ -1,13 +1,26 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
-  MongoClient.connect("mongodb+srv://stori:nodekurs19@cluster0-vy6oe.mongodb.net/test?retryWrites=true", { useNewUrlParser: true })
+  MongoClient.connect("mongodb+srv://stori:nodekurs19@cluster0-vy6oe.mongodb.net/shop?retryWrites=true", { useNewUrlParser: true })
     .then(client => {
       console.log("Connected!");
-      callback(client)
+      _db = client.db();
+      callback();
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+      console.log(err)
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if(_db) {
+    return _db;
+  }
+  throw 'No database found!';
+}
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
