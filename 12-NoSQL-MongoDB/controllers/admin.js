191f6ff12d4b3d43d4feaad61d40010d2958c908
Aleksandar Ristic <storiaca@gmail.com>
2019-03-08 15:50:26 +0100

    Working on the Product model to edit our product

--- 12-NoSQL-MongoDB/controllers/admin.js
+++ 12-NoSQL-MongoDB/controllers/admin.js
@@ -31,22 +31,19 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user
-    .getProducts({ where: { id: prodId } })
-    // Product.findById(prodId)
-    .then(products => {
-      const product = products[0];
-      if (!product) {
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product
-      });
-    })
-    .catch(err => console.log(err));
+    Product.findById(prodId)
+      .then(products => {
+        if (!products) {
+          return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+          pageTitle: 'Edit Product',
+          path: '/admin/edit-product',
+          editing: editMode,
+          product: products
+        });
+      })
+      .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
