c742a79c9a0331d498de7bf66264c862f54ee7ad
Aleksandar Ristic <storiaca@gmail.com>
2019-03-12 15:42:29 +0100

    Working on cart items and orders

--- 12-NoSQL-MongoDB/models/user.js
+++ 12-NoSQL-MongoDB/models/user.js
@@ -4,14 +4,27 @@ const { getDb } = require('../util/database');
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // {items: []}
   }
 
   save() {
     const db = getDb();
     return db.collection('users').insertOne(this)
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.itmes.findIndex(cp => {
+    //   return cp._id === product.id;
+    // });
+    const updateCart = { items: [{ ...product, quantity: 1 }]};
+    const db = getDb();
+    db.collection('users').updateOne(
+      {_id: new ObjectId(this)},
+      { $set: {cart: updateCart}}
+    )
   }
 
   static findById(userId) {
