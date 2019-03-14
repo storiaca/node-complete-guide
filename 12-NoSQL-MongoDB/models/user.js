697505b6b6cd3aac6ce8cdd4ef9f30d51950e2b9
Aleksandar Ristic <storiaca@gmail.com>
2019-03-14 15:16:03 +0100

    Dispalying cart items

--- 12-NoSQL-MongoDB/models/user.js
+++ 12-NoSQL-MongoDB/models/user.js
@@ -38,6 +38,24 @@ class User {
       )
   }
 
+  getCart() {
+    const db = getDb();
+    const productsIds = this.cart.items.map(i => {
+      return i.productId;
+    })
+    return db.collection('products')
+      .find({ _id: { $in: productsIds } })
+      .toArray()
+      .then(products => {
+        return products.map(p => {
+          return {
+            ...p, quantity: this.cart.items.find(i => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity
+          }
+        })
+      })
+  }
   static findById(userId) {
     const db = getDb();
     return db.collection('users').findOne({ _id: new ObjectId(userId) });
