9bf846c65d77dca2953a562ca37d29f8d2c896bc
Aleksandar Ristic <storiaca@gmail.com>
2019-03-15 15:55:35 +0100

    Adding an order

--- 12-NoSQL-MongoDB/models/user.js
+++ 12-NoSQL-MongoDB/models/user.js
@@ -70,6 +70,24 @@ class User {
       )
   }
 
+  addOrder() {
+    const db = getDb();
+    return db.collection('orders')
+      .insertOne(this.cart)
+      .then(results => {
+        this.cart = { items: []};
+        return db
+          .collection('users')
+          .updateOne(
+            { _id: new ObjectId(this._id) },
+            { $set: { cart: {items: [] } } }
+          )
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   static findById(userId) {
     const db = getDb();
     return db.collection('users').findOne({ _id: new ObjectId(userId) });
