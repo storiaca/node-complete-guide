cef485984f8a5bf6fba3a77061009374518a6b7d
Aleksandar Ristic <storiaca@gmail.com>
2019-03-18 12:38:21 +0100

    Connecting to the mongodb server with mongoose

--- 13-Mongoose/app.js
+++ 13-Mongoose/app.js
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5c87ad1e3441882287209034')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -33,9 +33,7 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
-  )
+  .connect('mongodb+srv://stori:nodekurs19@cluster0-vy6oe.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
   .then(result => {
     app.listen(3000);
   })
