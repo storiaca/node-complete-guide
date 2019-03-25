b992ad10db8bbc2f962b654cc027ff8c0eb775b1
Aleksandar Ristic <storiaca@gmail.com>
2019-03-25 15:28:43 +0100

    Initializing session middleware

--- 14-Sessions-and-Cookies/app.js
+++ 14-Sessions-and-Cookies/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -18,6 +19,11 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'my secret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use((req, res, next) => {
   User.findById('5c90db2b0dcd9f0419a5eae2')
