a7566802004d5a8dbc0c594708a29958fa0124c4
Aleksandar Ristic <storiaca@gmail.com>
2019-03-29 14:30:47 +0100

    Using the session middleware

--- 14-Sessions-and-Cookies/controllers/auth.js
+++ 14-Sessions-and-Cookies/controllers/auth.js
@@ -10,6 +10,6 @@ exports.getLogin = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true, HttpOnly')
+  req.session.isLoggedIn = true;
   res.redirect('/');
 }
