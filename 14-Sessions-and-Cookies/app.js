1918b46649f63ac0a717af067b41c268c2988d8f
Aleksandar Ristic <storiaca@gmail.com>
2019-03-29 15:12:45 +0100

    Using MongoDB to store a sessions

--- 14-Sessions-and-Cookies/app.js
+++ 14-Sessions-and-Cookies/app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://stori:nodekurs19@cluster0-vy6oe.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -22,7 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'my secret',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: false,
+  store: store
 }))
 
 app.use((req, res, next) => {
@@ -41,8 +49,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://stori:nodekurs19@cluster0-vy6oe.mongodb.net/shop?retryWrites=true', { useNewUrlParser: true })
+  .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
