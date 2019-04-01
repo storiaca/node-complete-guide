0fef4559aec319fb9351905d6dae08df97f6783f
Aleksandar Ristic <storiaca@gmail.com>
2019-04-01 15:09:54 +0200

    Fix add to cart button

--- 14-Sessions-and-Cookies/app.js
+++ 14-Sessions-and-Cookies/app.js
@@ -33,14 +33,17 @@ app.use(session({
   store: store
 }))
 
-// app.use((req, res, next) => {
-//   User.findById('5c90db2b0dcd9f0419a5eae2')
-//     .then(user => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  if(!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
