192c97b23b5e6abff69b3decfe060355a706a2d6
Aleksandar Ristic <storiaca@gmail.com>
2019-04-25 12:15:29 +0200

    Checking for field equality

--- 18-Understanding Validation/routes/auth.js
+++ 18-Understanding Validation/routes/auth.js
@@ -24,7 +24,13 @@ router.post("/signup",
       }),
       body('password', 'Please enter a password with only numbers and text at least 5 characters.')
         .isLength({min: 5})
-        .isAlphanumeric()
+        .isAlphanumeric(),
+      body('confirmPassword').custom((value, {req}) => {
+        if(value !== req.body.password) {
+          throw new Error('Password have to match!');
+        }
+        return true;
+      }) 
   ], 
   authController.postSignup);
 
