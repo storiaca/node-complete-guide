9db3e589bb0cc3ed024ccf98090e8692f2668f0b
Aleksandar Ristic <storiaca@gmail.com>
2019-08-06 15:27:44 +0200

    Errors - Some Theory

---
+++ 19-Error-Handling/error-playground.js
@@ -0,0 +1,14 @@
+const sum = (a, b) => {
+  if (a && b) {
+    return a + b;
+  }
+  throw new Error("Invalid arguments");
+};
+try {
+  console.log(sum(1));
+} catch (error) {
+  console.log("Error occured");
+  //console.log(error);
+}
+
+console.log("This works");
