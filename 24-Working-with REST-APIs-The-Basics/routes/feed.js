c4cf5e33b7afe2925c408cece7cf5b3cd5f1f0ff
Aleksandar Ristic <storiaca@gmail.com>
2019-11-12 16:01:52 +0100

    Sending request and responses and working with Postman

--- 24-Working-with REST-APIs-The-Basics/routes/feed.js
+++ 24-Working-with REST-APIs-The-Basics/routes/feed.js
@@ -7,5 +7,9 @@ const router = express.Router();
  * GET /feed/posts
  */
 router.get("/posts", feedController.getPosts);
+/**
+ * POST /feed/post
+ */
+router.post("/post", feedController.createPost);
 
 module.exports = router;
