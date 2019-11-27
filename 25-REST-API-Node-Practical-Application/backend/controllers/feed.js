8e25720db995b3cf42cf62f76154a53146c00991
Aleksandar Ristic <storiaca@gmail.com>
2019-11-27 10:36:49 +0100

    clearing Post-User relations

--- 25-REST-API-Node-Practical-Application/backend/controllers/feed.js
+++ 25-REST-API-Node-Practical-Application/backend/controllers/feed.js
@@ -167,7 +167,13 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndDelete(postId);
     })
     .then(result => {
-      console.log(result);
+      return User.findById(req.userId);
+    })
+    .then(user => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then(result => {
       res.status(200).json({ message: "Delete post." });
     })
     .catch(err => {
