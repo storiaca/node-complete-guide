880f3d0cc8e72f61bb0d8eaccbb138f4fc3bf247
Mac <storiaca@gmail.com>
2020-02-07 15:42:37 +0100

    Deleting Posts Across Clients

--- 27-Websockets-and-socket-io/27-backend/controllers/feed.js
+++ 27-Websockets-and-socket-io/27-backend/controllers/feed.js
@@ -165,7 +165,7 @@ exports.deletePost = async (req, res, next) => {
 
     user.posts.pull(postId);
     await user.save();
-
+    io.getIO().emit("posts", { action: "delete", post: postId });
     res.status(200).json({ message: "Delete post." });
   } catch (err) {
     if (!err.statusCode) {
